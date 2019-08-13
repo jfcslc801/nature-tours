4baf47a9b7cf518e4dd6694dc5a01ab3814b212d
jfrancoc <jfcslc@outlook.com>
2019-08-12 21:12:00 -0600

    tours post route

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+// Json middleware
+app.use(express.json());
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
 
-// tours route
+// tours get route
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'Success',
@@ -19,6 +21,30 @@ app.get('/api/v1/tours', (req, res) => {
 
 });
 
+// tours post route
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+    // new tour id
+    const newId = tours[tours.length - 1].id + 1;
+    // new tour 
+    const newTour = Object.assign({ id: newId }, req.body);
+    // push new tour to array
+    tours.push(newTour);
+    // write file
+    fs.writeFile(
+        `${__dirname} / dev - data / data / tours - simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'Success',
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+    );
+
+});
 
 
 const port = 3000;

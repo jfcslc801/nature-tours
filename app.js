22742a3241864aa009265624d02bf7ee6078614b
jfrancoc <jfcslc@outlook.com>
2019-08-12 20:39:13 -0600

    tours route

---
+++ app.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+// tours route
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'Success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+
+});
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
+});

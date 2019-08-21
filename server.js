aa466019286939c879421e35a63ab4eb7d90dd6d
jfrancoc <jfcslc@outlook.com>
2019-08-21 14:29:37 -0600

    eslint config

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = require('./app');
 // START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });

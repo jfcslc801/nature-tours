74e9b6e4f7198127be8bf440e98c6aafe9b2c0fd
jfrancoc <jfcslc@outlook.com>
2019-08-22 21:59:51 -0600

    mongoose setup

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes')
+const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes')
+;
 const app = express();
 
 // MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
-
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
-
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+// mongoose db
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+// mongoose connect
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection Successful!'));
+
+// Tour Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  rating: Number,
+  price: Number
+});
 // START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

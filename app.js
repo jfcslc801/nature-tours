e52a7504dc1666a891d3430972c4b968f6337d82
jfrancoc <jfcslc@outlook.com>
2019-10-03 18:42:16 -0600

    global error handler

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,21 +20,10 @@ app.use('/users', userRouter);
 
 // bad link error handler
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`));
 });
 
 // global error handler
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;

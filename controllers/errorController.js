eb371e233cc6465002cdc4e8a86e4aef15fdd5c2
jfrancoc <jfcslc@outlook.com>
2019-10-04 12:52:19 -0600

    validation error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -13,6 +13,13 @@ const handleDuplicateFieldsDB = err => {
   const message = `Duplicate field value: ${value}. Please enter unique value.`;
   return new AppError(message, 400);
 };
+
+// validation error
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 // development error
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -52,6 +59,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };

f3d5dfaef23bdb6b3733c9b8b019c7867bd10481
jfrancoc <jfcslc@outlook.com>
2019-10-08 13:19:30 -0600

    json web token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
 
 exports.signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,6 +11,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
+  // json web token
   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
   });
@@ -20,5 +22,26 @@ exports.signUp = catchAsync(async (req, res, next) => {
     data: {
       user: newUser
     }
+  });
+});
+
+// login
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // check email and password exists
+  if (!email || !password) {
+    next(new AppError('Please provide valid email and password!', 400));
+  }
+
+  // check if user exists and password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  console.log(user);
+
+  // if all is authenticated, send web token
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token
   });
 });

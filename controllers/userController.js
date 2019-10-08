2b961948b33387f7658813e954303018db61f3b4
jfrancoc <jfcslc@outlook.com>
2019-10-08 13:19:03 -0600

    get all users route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
-exports.getAllUsers = (req, res) => {
-  // respose for deleted tour
-  res.status(500).json({
-    status: 'Error',
-    message: 'This route not implemented'
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'Success',
+    results: users.length,
+    data: {
+      users
+    }
   });
-};
+});
 exports.getUser = (req, res) => {
   // respose for deleted tour
   res.status(500).json({

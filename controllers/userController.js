a49d70381ee251d771d1e6581d27e9d248b25f05
jfrancoc <jfcslc@outlook.com>
2019-10-16 13:16:05 -0600

    update my account

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+// filter body object
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -11,6 +22,33 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.updateMyAccount = catchAsync(async (req, res, next) => {
+  // create error if user updates password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'Unable to update password. Please visit tours/updatePassword!',
+        400
+      )
+    );
+  }
+
+  // filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  // update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
 exports.getUser = (req, res) => {
   // respose for deleted tour
   res.status(500).json({

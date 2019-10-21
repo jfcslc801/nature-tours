ea656ab5aa3118aa0d5bb33c4f887c4f87da3219
jfrancoc <jfcslc@outlook.com>
2019-10-21 09:55:48 -0600

    ip rate limiter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -11,6 +12,14 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// IP rate limiter
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an hour!'
+});
+app.use('/', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 

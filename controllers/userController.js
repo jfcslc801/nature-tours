8f2f68d9e1bef85f734f175ef9c3d61c7abd8d4b
jfrancoc <jfcslc@outlook.com>
2019-10-24 16:28:14 -0600

    create user error note

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,7 +52,7 @@ exports.createUser = (req, res) => {
   // respose for deleted tour
   res.status(500).json({
     status: 'Error',
-    message: 'This route not implemented'
+    message: 'This route not implemented, please use sign up'
   });
 };
 

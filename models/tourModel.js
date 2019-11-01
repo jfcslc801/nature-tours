0da1741dc5bbaac17fb94020cc2cff125177f3d9
jfrancoc <jfcslc@outlook.com>
2019-11-01 17:32:28 -0600

    round ratings to closest tenth

--- models/tourModel.js
+++ models/tourModel.js
@@ -38,7 +38,8 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
+      max: [5, 'Rating must be below 5.0'],
+      set: val => Math.round(val * 10) / 10 // 4.3333, 43, 4.3
     },
     ratingQuantity: {
       type: Number,

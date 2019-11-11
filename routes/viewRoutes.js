0314e4f9efe8e9af7f6c4dd2e1db10372c697b5f
jfrancoc <jfcslc@outlook.com>
2019-11-11 13:42:45 -0700

    view routes setup

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+router.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+router.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker.'
+  });
+});
+
 module.exports = router;

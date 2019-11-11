ea87e6e5bf705fd3ae0a40007c1b44242a3acf8a
jfrancoc <jfcslc@outlook.com>
2019-11-11 13:41:21 -0700

    tour  page setup

--- app.js
+++ app.js
@@ -72,6 +72,16 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.status(200).render('base');
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker.'
+  });
+});
 app.use('/tours', tourRouter);
 app.use('/users', userRouter);
 app.use('/reviews', reviewRouter);

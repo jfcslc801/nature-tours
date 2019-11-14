43a2376d4ea1519f039e804b14a480a04a69d188
jfrancoc <jfcslc@outlook.com>
2019-11-14 15:31:31 -0700

    map settings

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -5,5 +5,8 @@ console.log(locations);
 mapboxgl.accessToken = 'pk.eyJ1IjoiamZjc2xjODAxIiwiYSI6ImNrMno5azdoejAzNGEzZHA2OHlseHRxbGwifQ.t7L063ARRCSJsNHamna1Lw';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+  style: 'mapbox://styles/jfcslc801/ck2z9zcob0a431co95ogzzhfa',
+  center: [-111.896821, 40.762864],
+  zoom: 6,
+  interactive: false
 });
